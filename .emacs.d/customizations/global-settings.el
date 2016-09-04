0c76d09007535414f83b024fc251ed11e46f9eee
thomasodot <tom@ip-172-31-38-43.eu-west-1.compute.internal>
2016-09-04 11:17:45 +0000

    Saves and backups going into a backsups dir in the .emacs.d dir now.

--- .emacs.d/customizations/global-settings.el
+++ .emacs.d/customizations/global-settings.el
@@ -1,6 +1,21 @@
 (global-linum-mode 1)
 (setq linum-format "%4d \u2502 ")
 
+;; When you visit a file, point goes to the last place where it
+;; was when you previously visited the same file.
+;; http://www.emacswiki.org/emacs/SavePlace
+(require 'saveplace)
+(setq-default save-place t)
+;; keep track of saved places in ~/.emacs.d/places
+(setq save-place-file (concat user-emacs-directory "places"))
+
+;; Emacs can automatically create backup files. This tells Emacs to
+;; put all backups in ~/.emacs.d/backups. More info:
+;; http://www.gnu.org/software/emacs/manual/html_node/elisp/Backup-Files.html
+(setq backup-directory-alist `(("." . ,(concat user-emacs-directory
+                                               "backups"))))
+(setq auto-save-default nil)
+
 ;; (setq
 ;; backup-by-copying t                    ; don't clobber symlinks
 ;; backup-directory-alist
@@ -9,13 +24,13 @@
 ;;  kept-new-versions 6
 ;;  kept-old-versions 2
 ;; version-control t)                     ; use versioned backups
-
+;; 
 ;; (setq auto-save-file-name-transforms
-      ;; `((".*" "~/.saves" t)))
+;; ;;       `((".*" "~/.saves" t)))
 
 ;; Autoindent open-*-lines
-;; (defvar newline-and-indent t
-;; "Modify the behavior of the open-*-line functions to cause them to autoindent.")
+(defvar newline-and-indent t
+"Modify the behavior of the open-*-line functions to cause them to autoindent.")
 
 (require 'evil)
 (evil-mode 1)
